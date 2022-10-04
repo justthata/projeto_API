0be915eaafc62bbc87e64a3d992ba682b3e75f59
Bruno Costa <analistabrunocosta@gmail.com>
2022-10-03 21:00:19 -0300

    Avançando em API

--- app/controller/produtoController.php
+++ app/controller/produtoController.php
@@ -3,15 +3,15 @@ namespace App\controller;// utilizando namespace para identificar nossa classe
 
 class ProdutoController{
     public function get(){
-
+        return "Estou no método get";
     }
     public function post(){
-
+        return "Estou no método post";
     }
     public function put($id){
-
+        return "Estou no método put";
     }
     public function delete($id){
-
+        return "Estou no método delete";
     }
 }
--- index.php
+++ index.php
@@ -27,11 +27,18 @@ require_once("vendor/autoload.php");
         else{
             $servico = "App\controller\\".ucfirst($rota[0])."Controller";// Organizando o caminho para deixar com o mesmo nome da classe da pasta controller
             echo"<hr> {$servico}";
-
+        
             array_shift($rota);
-            $verboHTTP = $_SERVER["REQUEST_METHOD"];// está pegando o verbo HTTP, que é o método de envio dos dados, ou seja, GET, POST, PUT e DELETE
+            $verboHTTP = strtolower($_SERVER["REQUEST_METHOD"]);// está pegando o verbo HTTP, que é o método de envio dos dados, ou seja, GET, POST, PUT e DELETE
+            echo "<hr> {$verboHTTP}";
+            try {
+                array_shift($rota);
+                $resposta = call_user_func_array(array(new $servico, $verboHTTP), $rota);// Estamos instanciando  dinamicamente a classe ProdutoController e informando qual método da classe será executado, passando a variável $verboHTTP, que será get, post, put e delete. A variável $rota, será utilizada para passar o parâmetro para o método da nossa classe, seria algo como delete(1) ou put(1).
 
-
+                echo $resposta;
+            } catch (\Exception $erro) {
+                
+            }
         }
 
        
